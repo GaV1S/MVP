67ace745f6cfeba12c6674e4548bc85154ecd7cb
GaV1S <gav1s@yandex.ru>
2022-04-02 09:00:32 +0300

    first commit

---
+++ app/src/main/java/ru/s1aks/mvp_login_activity/MainActivity.kt
@@ -0,0 +1,11 @@
+package ru.s1aks.mvp_login_activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
